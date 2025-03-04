7e6eac841c01321a55df200a14f006708bda4aed
Tonysoossa <tony.servanin@gmail.com>
2025-03-04 20:14:48 +0100

    resizing the form

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -22,7 +22,7 @@ export default function Contact() {
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    
+
     const templateParams = {
       name: formData.name,
       email: formData.email,
@@ -52,41 +52,45 @@ export default function Contact() {
   };
 
   return (
-    <div className="max-w-2xl mx-auto relative">
-      <h1 className="text-4xl font-bold mt-14 mb-20 text-center">
+    <div className="relative">
+      <h1 className="text-4xl font-bold mt-14 mb-20 flex text-center justify-center">
         {translations.contact.title}
       </h1>
-      <form onSubmit={handleSubmit} className="space-y-8">
-        <div>
-          <label htmlFor="name" className="block text-sm font-medium mb-2">
-            {translations.contact.name}
-          </label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            value={formData.name}
-            onChange={handleChange}
-            required
-            className="w-full px-3 py-2 bg-gray-800 border border-gray-700 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-          />
+
+      <form onSubmit={handleSubmit} className="space-y-8 flex flex-col text-xl">
+        <div className="flex flex-row gap-12 justify-center w-full">
+          <div className="w-full">
+            <label htmlFor="name" className="block lg:text-[1.2rem] text-sm font-medium mb-4">
+              {translations.contact.name}
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              required
+              className="w-full h-12 px-3 py-2 bg-gray-800 border border-gray-700 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+          </div>
+          <div className="w-full">
+            <label htmlFor="email" className="block lg:text-[1.2rem] text-sm font-medium mb-4">
+              {translations.contact.mail}
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+              className="w-full h-12 px-3 py-2 bg-gray-800 border border-gray-700 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+          </div>
         </div>
+
         <div>
-          <label htmlFor="email" className="block text-sm font-medium mb-2">
-            {translations.contact.mail}
-          </label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            required
-            className="w-full px-3 py-2 bg-gray-800 border border-gray-700 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-          />
-        </div>
-        <div>
-          <label htmlFor="message" className="block text-sm font-medium mb-2">
+          <label htmlFor="message" className="block lg:text-[1.2rem] text-sm font-medium mb-4">
             {translations.contact.message}
           </label>
           <textarea
@@ -96,12 +100,13 @@ export default function Contact() {
             onChange={handleChange}
             required
             rows={4}
-            className="w-full px-3 py-2 bg-gray-800 border border-gray-700 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+            className="w-full h-48 px-3 py-2 bg-gray-800 border border-gray-700 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
           ></textarea>
         </div>
+
         <button
           type="submit"
-          className="w-full bg-blue-600 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded transition-colors"
+          className="w-2/5 mx-auto bg-blue-600 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded transition-colors"
         >
           {translations.contact.submit}
         </button>
